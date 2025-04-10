d6b198b8bc55032652226eb3d8c4075ddbeadb65
Talha Azaz <talha129c@gmail.com>
2025-04-10 06:24:40 -0500

    add options

--- floability-env-cli.sh
+++ floability-env-cli.sh
@@ -37,7 +37,7 @@ cmd="python code_injection.py $notebook $password"
 eval "$cmd"
 
 echo "Running the notebook at path: $notebook with kernel: $kernel."
-cmd="jupyter notebook --MultiKernelManager.default_kernel_name=$kernel $notebook"
+cmd="jupyter notebook --MultiKernelManager.default_kernel_name=$kernel $notebook --no-browser --ip=0.0.0.0"
 eval "$cmd"
 echo "Successfully completed notebook execution and auditing."
 trap - SIGINT
