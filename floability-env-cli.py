19d89830ebc7b3b9a332459e4c01b60e31344429
Talha Azaz <talha129c@gmail.com>
2025-05-07 21:08:25 -0500

    generate manager and worker dependencies

--- floability-env-cli.py
+++ floability-env-cli.py
@@ -20,6 +20,7 @@ def get_parsed_arguments():
 
 strace_worker = os.getcwd()+"/strace_worker.txt"
 strace_manager = os.getcwd()+"/strace_manager.txt"
+open_trace_log = os.getcwd()+"/open_trace.log"
 
 args = get_parsed_arguments()
 if args.notebook:
@@ -33,6 +34,51 @@ else:
 	notebook_copy_path = "copy_" + notebook_name
 shutil.copy(notebook_path, notebook_copy_path)
 print("Created temp copy of the notebook: ", notebook_name)
+
+# add code to the top of the notebook to capture data depencies
+code_to_add = f"""
+import builtins
+import os
+
+log_file = "{open_trace_log}"
+if os.path.exists(log_file):
+    os.remove(log_file)
+
+def open(file, mode='r', *args, **kwargs):
+    with builtins.open(log_file, "a") as log:
+        log.write(file + "\\n")
+    return builtins.open(file, mode, *args, **kwargs)
+
+original_open = builtins.open
+def traced_open(file, mode='r', *args, **kwargs):
+    with original_open(log_file, "a") as log:
+        log.write(file + "\\n")
+    return original_open(file, mode, *args, **kwargs)
+builtins.open = traced_open
+"""
+
+# use nbformat to add the code to the top of the notebook
+import nbformat
+
+def add_code_to_notebook(notebook_path, code):
+    """
+    Add code to the top of a Jupyter notebook.
+    """
+    with open(notebook_path, 'r') as f:
+        nb = nbformat.read(f, as_version=4)
+
+    # Create a new code cell
+    new_cell = nbformat.v4.new_code_cell(code)
+    
+    # Insert the new cell at the beginning
+    nb.cells.insert(0, new_cell)
+
+    # Write the modified notebook back to the file
+    with open(notebook_path, 'w') as f:
+        nbformat.write(nb, f)
+
+add_code_to_notebook(notebook_copy_path, code_to_add)
+print("Added code to the top of the notebook.")
 
 print("Starting vine workers with strace...")
 p_worker = subprocess.Popen(['strace', '-qqq', '-r', '-z', '-f', '-o', strace_worker, 'vine_worker', 'localhost', '9123'], start_new_session=True)
@@ -53,4 +99,9 @@ print("Removed vine worker process tree.")
 
 # find the dependencies and geneate yaml file
 p = subprocess.Popen(['python', 'generate_requirements.py', strace_manager, strace_worker])
+p.wait()
+
+
+# find data dependencies and generate txt file
+p = subprocess.Popen(['python', 'generate_data_dep.py', open_trace_log])
 p.wait()
---
+++ generate_data_dep.py
@@ -0,0 +1,65 @@
+
+def process_strace_log(file_path, data_dep_list):
+    deps = []
+    seen = set()
+    
+    try:
+        with open(file_path, 'r') as file:
+            for line in file:
+                if 'openat' not in line:
+                    continue
+                
+                start = line.index('"') + 1
+                end = line.index('"', start)
+                full_path = line[start:end]
+                
+                # check if path is in data_dep_list
+                if not any(dep.startswith(full_path) for dep in data_dep_list):
+                    continue
+                # check if path is in seen
+                if full_path in seen:
+                    continue
+                seen.add(full_path)
+        return seen
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found")
+        return set()
+    except Exception as e:
+        print(f"Error processing file: {str(e)}")
+        return set()
+
+def get_list_of_files(file_path):
+    # Read the list of files from the file
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+        lines = [line.strip() for line in lines]
+        lines = [line for line in lines if line and not line.startswith('/proc') and not line.startswith('/sys') and 'site-packages' not in line and 'vine-run-info' not in line and 'open_trace.log' not in line]
+    return lines
+
+
+import sys
+import os
+
+if __name__ == "__main__":
+    # Get the list of data dependencies from the open_trace.log file
+
+    file_path = sys.argv[1] if len(sys.argv) > 1 else 'open_trace.log'
+    if not os.path.exists(file_path):
+        print(f"Error: File '{file_path}' not found")
+        sys.exit(1)
+
+    data_dep_list = get_list_of_files('open_trace.log')
+
+    # Process the strace logs for manager and worker
+    manager_list = process_strace_log('strace_manager.txt', data_dep_list)
+    worker_set = process_strace_log('strace_worker.txt', data_dep_list)
+
+    # Write manager dependencies to a file
+    with open('manager_data_dependencies.txt', 'w') as manager_file:
+        for item in manager_list:
+            manager_file.write(f"{item}\n")
+
+    # Write worker dependencies to a file
+    with open('worker_data_dependencies.txt', 'w') as worker_file:
+        for item in worker_set:
+            worker_file.write(f"{item}\n")
